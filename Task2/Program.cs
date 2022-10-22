48801ca0782e911ce646b38a177868e195961c32
Your Name <you@example.com>
2022-10-22 10:50:02 +0300

    Задание выполнены

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Написать программу масштабирования фигуры
+
+double[] massiv_coordinat_s_mashtabom(string[] num, double k)
+{
+    double[] mas = new double[num.Length];
+    for (int ind = 0; ind < num.Length; ind++)
+    {
+        mas[ind] = (double.Parse(num[ind])) * k;
+    }
+    return mas;
+}
+
+void Pechat_Massiva(double[] massiv)
+{
+    for (int i = 0; i < massiv.Length; i+=2)
+    {
+        Console.Write($"({massiv[i]}, {massiv[i+1]})  ");
+    }
+}
+
+Console.Write("Введите координаты точек фигуры через запятую ',': ");
+string[] num = (Console.ReadLine() ?? "0").Split(',');
+Console.Write("Введите коэфициент масштабирования: ");
+double k = double.Parse(Console.ReadLine() ?? "0");
+double[] mash_koor = massiv_coordinat_s_mashtabom(num, k);
+Pechat_Massiva(mash_koor);
---
+++ Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿//Написать программу копирования массива
+void Zapolnenie_Massiva(int[] massiv)
+{
+    Random Znach = new Random();
+    for (int ind = 0; ind < massiv.Length; ind++)
+    {
+        massiv[ind] = Znach.Next(-9, 10);
+    }
+}
+void Pechat_Massiva(int[] massiv)
+{
+    foreach (var item in massiv)
+    {
+        Console.Write($"{item} ");
+    }
+}
+int[] Kopirovanie_massiva(int[] masiv)
+{
+    int[] Kopmas = new int [masiv.Length];
+    for (int ind = 0; ind < masiv.Length; ind++)
+    {
+        Kopmas[ind] = masiv[ind];
+    }
+    return Kopmas;
+}
+Console.Write("Введите длинну массива: ");
+int dlinna = int.Parse(Console.ReadLine()?? "0");
+int[] Array = new int[dlinna];
+Zapolnenie_Massiva(Array);
+Console.Write("Начальный массив: ");
+Pechat_Massiva(Array);
+Console.Write("Скопированный массив: ");
+int[] Array_copy = Kopirovanie_massiva(Array);
+Pechat_Massiva(Array_copy);
